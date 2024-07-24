94cb3c93e6c67e431b83026011c061e4e4616980
CyberCatStudios <gwaine777@gmail.com>
2024-07-24 15:15:57 -0400

    added Python file
    Added the literal program

---
+++ robux.generate.hack.give-99999.py
@@ -0,0 +1,67 @@
+#importing
+import time
+
+import subprocess
+import sys
+from os import system, name
+#package installer >:3
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+#install mysql connector for highly unsuspicious reasons
+install("mysql-connector-python")
+
+#clearscreen function
+def clear():
+ 
+    if name == 'nt':
+        _ = system('cls')
+ 
+    else:
+        _ = system('clear')
+
+#the fun part
+
+#inefficent code lol
+clear()
+print("Generating 99999 robux")
+time.sleep(1)
+clear()
+print("Generating 99999 robux.")
+time.sleep(1)
+clear()
+print("Generating 99999 robux..")
+time.sleep(1)
+clear()
+print("Generating 99999 robux...")
+time.sleep(1)
+clear()
+print("Generating 99999 robux")
+time.sleep(1)
+clear()
+print("Generating 99999 robux.")
+time.sleep(1)
+clear()
+print("Generating 99999 robux..")
+time.sleep(1)
+clear()
+print("Generating 99999 robux...")
+time.sleep(1)
+clear()
+print("Generating 99999 robux")
+time.sleep(1)
+clear()
+print("Generating 99999 robux.")
+time.sleep(1)
+clear()
+print("Generating 99999 robux..")
+time.sleep(1)
+clear()
+print("Generating 99999 robux...")
+time.sleep(1)
+clear()
+#useless text
+print("Robux successfully generated.")
+time.sleep(0.6)
+print("Please wait 5-10 minutes for stack to reload.")
+time.sleep(0.6)
